44c981e48bc7c4e6ada1f98a44e93a0f1abe75e5
Wojciech Sabat <wojciech.sabat@clxcommunications.com>
2020-02-04 16:55:02 +0100

    Initialized repository

---
+++ AnotherOrigin/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AnotherOrigin.Models;
+
+namespace AnotherOrigin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Test()
+        {
+            return Ok($"Authorization header value is '{Request.Headers["Authorization"]}'.");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ BaseOrigin/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿console.log("I'm in");
+let xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://localhost:5004/home/test\n', true);
+xhr.setRequestHeader("Authorization", "secret");
+//xhr.withCredentials = true;
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        alert("The response is: " + xhr.responseText);
+    }
+};
+xhr.send(null);
